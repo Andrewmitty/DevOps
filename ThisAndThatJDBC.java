09e04b9a6d78fc93450d18f64f3ed353e693b5a1
Matthew Thompson Soto <61574190+thompsoto@users.noreply.github.com>
2023-03-01 16:55:19 -0600

    Add files via upload

--- ThisAndThatJDBC.java
+++ ThisAndThatJDBC.java
@@ -10,9 +10,9 @@ class ThisAndThatJDBC{
             while(rsThat.next()){
                 int foreignKeyToThis = rsThat.getInt(4);
                 System.out.println(rsThat.getInt(1)
-                    + " " + rsThis.getString(2)
-                    + " " + rsThis.getString(3)
-                    + " " + rsThis.getInt(4));
+                    + " " + rsThat.getString(2)
+                    + " " + rsThat.getString(3)
+                    + " " + rsThat.getInt(4));
 
                 String thisQuery = "select * from This where This_pk = '" + foreignKeyToThis + "'";
                 System.out.println(thisQuery);
